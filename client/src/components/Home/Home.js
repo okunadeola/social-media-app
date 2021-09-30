303679fc04cf5a47e335ba053c290738688cd321
okunade olaitan <olaokunade@gmail.com>
2021-09-30 18:32:16 +0100

    first commit

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import { Grid, Grow, Container } from '@material-ui/core'
+import Form from '../Form/Form'
+import Posts from '../Posts/Posts'
+import { useDispatch } from 'react-redux'
+import { getPosts } from '../../actions/posts'
+
+const Home = () => {
+  const [currentId, setCurrentId] = useState(null)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getPosts())
+  }, [currentId, dispatch])
+
+  return (
+    <Grow in>
+      <Container>
+        <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+          <Grid item xs={12} sm={7}>
+            <Posts setCurrentId={setCurrentId} />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Form currentId={currentId} setCurrentId={setCurrentId} />
+          </Grid>
+        </Grid>
+      </Container>
+    </Grow>
+  )
+}
+
+export default Home
---
+++ client/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import { Provider } from 'react-redux'
+import { createStore, applyMiddleware } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk'
+import reducers from './reducers'
+
+import App from './App'
+
+const store = createStore(reducers, composeWithDevTools(applyMiddleware(thunk)))
+
+ReactDOM.render(<Provider store={store}> <App /></Provider>, document.getElementById('root'))
